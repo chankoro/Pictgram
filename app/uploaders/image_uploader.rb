2a24a42650a7a85caa7f9c2a233f076438b82b4f
youhei yamada <eagle.ff1214@icloud.com>
2021-08-09 05:55:00 +0000

    Rails06課題

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,6 +32,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
+  
+  def size_range
+    1..10.megabytes
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -39,9 +43,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
 
+ def extension_allowlist
+    %w(jpg jpeg png)
+ end
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+ 
 end
