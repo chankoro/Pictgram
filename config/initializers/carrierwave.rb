7f0928b4b5300290823136335ee08f5238daf497
youhei yamada <eagle.ff1214@icloud.com>
2021-08-14 04:39:54 +0000

    initializers/carrierwave.rbをいじった

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,13 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory = ENV['AWS_BUCKET']
+  config.asset_host ='https://s3.amazonaws.com/samplepicture1234'
+  config.fog_public = false
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
+    aws_access_key_id: 'AWS_ACCESS_KEY_ID',
+    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+    region: 'ap-northeast-1',
     path_style: true
   }
-end
+  
